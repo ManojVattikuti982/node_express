17b652232371dc0ecd13b211e5690befbf493ec9
ManojVattikuti982 <manojvattikuti2000@gmail.com>
2025-09-02 16:55:28 +0530

    Initial commit: setup express, mongoose, product model

---
+++ simple-app/backend/src/app.js
@@ -0,0 +1,51 @@
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+let app = express();
+
+//db connection
+async function dbConnect() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("db connected");
+  } catch (error) {
+    console.log("db connection error", error.message);
+  }
+}
+dbConnect();
+
+//schema for simple-app
+const productSchema = new mongoose.Schema(
+  {
+    productName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: String,
+      enum: ["stationery", "kitchen", "electronics", "apparels"],
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      default: 0,
+    },
+    isFastMoving: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }//createdAt,updatedAt
+); 
+
+//model
+let Product=mongoose.model("Product",productSchema)
+
+
+
+export default app;
---
+++ simple-app/backend/src/index.js
@@ -0,0 +1,11 @@
+import http from 'http'
+import app from './app.js';
+import "dotenv/config"; 
+
+let PORT=process.env.PORT || 3000;
+let server=http.createServer(app);
+
+
+server.listen(PORT,()=>{
+    console.log(`Server is on ${PORT}...`);
+})
